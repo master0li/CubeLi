66c531df5c8441aa4383457ef172d92cd390f7c4
Charles Li <masterli@charlesli.com>
2020-02-07 01:50:37 -0600

    firebase config

--- src/src/environments/environment.ts
+++ src/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAZ_-NAVtttpKnwFTt4dJIkpyMo0o9W5qQ",
+    authDomain: "cubeli.firebaseapp.com",
+    databaseURL: "https://cubeli.firebaseio.com",
+    projectId: "cubeli",
+    storageBucket: "cubeli.appspot.com",
+    messagingSenderId: "869777506672",
+    appId: "1:869777506672:web:5dab273a19adf2cfde0fb5",
+    measurementId: "G-R703C4Y7J4"
+  }
 };
 
 /*
