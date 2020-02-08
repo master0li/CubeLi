8628ebc46816f1244b6305e251ee3df89bcb7c77
Charles Li <masterli@charlesli.com>
2020-02-08 11:00:33 -0600

    firebase config in prod

--- src/src/environments/environment.prod.ts
+++ src/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,firebase: {
+    apiKey: "AIzaSyAZ_-NAVtttpKnwFTt4dJIkpyMo0o9W5qQ",
+    authDomain: "cubeli.firebaseapp.com",
+    databaseURL: "https://cubeli.firebaseio.com",
+    projectId: "cubeli",
+    storageBucket: "cubeli.appspot.com",
+    messagingSenderId: "869777506672",
+    appId: "1:869777506672:web:5dab273a19adf2cfde0fb5",
+    measurementId: "G-R703C4Y7J4"
+  }
+
+
 };
